de9b6a2d74aa302fba4a05c6202c807cd2a65b2a
Steven Hsu <admin@stevenhsu.ca>
2013-11-16 18:20:06 -0800

    Removed redundant sample connection code.

--- connect-stackato.php
+++ connect-stackato.php
@@ -1,21 +1,12 @@
 <?php
-
     $services = getenv("VCAP_SERVICES");
     $services_json = json_decode($services,true);
     $couchdb_conf = $services_json["couchdb"][0]["credentials"];
-    echo "http://".$couchdb_conf["username"].":"
-      .$couchdb_conf["password"]."@".$couchdb_conf["host"].":"
-      .$couchdb_conf["port"];
 
-
-    ?><hr /><?php
-    echo $couchdb_conf["name"];
-
-
-$couch_dsn = "http://".$couchdb_conf["username"].":"
+    $couch_dsn = "http://".$couchdb_conf["username"].":"
       .$couchdb_conf["password"]."@".$couchdb_conf["host"].":"
       .$couchdb_conf["port"];  									// database host:port
-$couch_db = $couchdb_conf["name"];  	// "tasks"?			// database name
+    $couch_db = $couchdb_conf["name"];  	// "tasks"?			// database name
 
 require_once "./lib/couch.php";
 require_once "./lib/couchClient.php";
